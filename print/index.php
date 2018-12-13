e608b373c97b38c5e444d8532a71a67acf08043c
kaurkaelepkhk <kaur.kaelep@khk.ee>
2018-12-13 13:06:52 +0200

    väljastamine

---
+++ print/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaur.kaelep
+ * Date: 13.12.2018
+ * Time: 12:46
+ */
+
+// Teksti väljastamine
+
+// print
+print('Hello World!<br>');
+print 'PHP Alused <br>';
+// echo
+echo 'Nii toimub väljastamine<br>';
+// väärtuste ühendamine
+echo 'See '.'on'.' kolmest sõnast kokku pandud<br>';
+?>
