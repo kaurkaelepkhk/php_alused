bf87bc5cb91196f52d799ed0f715323901698c80
kaurkaelepkhk <kaur.kaelep@khk.ee>
2018-12-18 15:08:25 +0200

    Massiividega töötamine

--- arrays/index.php
+++ arrays/index.php
@@ -7,9 +7,42 @@
  */
 
 // Andmemasiivi loomine
-$kasutajad = array(
+/*$kasutajad = array(
     'Mike',
     'Diane',
     'Superman',
     'HenryTheThird',
-);
+);*/
+
+//Väljastamine foriga
+/*for ($i = 0; $i < count($kasutajad); $i++) {
+    echo $i.' - '.$kasutajad[$i].'<br>';
+}*/
+
+//Väljastamine forEachiga
+/*foreach($kasutajad as $kasutaja) {
+    echo $kasutaja.'<br>';
+}
+//Massiivi andmete sisestamine
+
+$kasutajad = array();
+
+$kasutajad[] = 'Mike';
+$kasutajad[] = 'Superman';
+array_push($kasutajad, 'Diane');
+
+// Massiivi andmete eemaldamine
+
+// tagantpoolt
+
+array_pop($kasutajad);
+
+// eestpoolt
+
+array_shift($kasutajad);
+
+// array sisu checkimine
+
+print_r
+var_dump
+*/
