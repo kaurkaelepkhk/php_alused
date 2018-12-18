87068b7477fbccf3d3a3fbacdf1340fcb97e5bf4
kaurkaelepkhk <kaur.kaelep@khk.ee>
2018-12-18 14:49:45 +0200

    Massiivid

---
+++ arrays/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaur.kaelep
+ * Date: 18.12.2018
+ * Time: 14:42
+ */
+
+// Andmemasiivi loomine
+$kasutajad = array(
+    'Mike',
+    'Diane',
+    'Superman',
+    'HenryTheThird',
+);
